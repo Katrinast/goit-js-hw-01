b112d9f5e7eb58df3da183db9b929be05b240398
Katrinast <stehnovicekaterina@gmail.com>
2024-02-28 20:00:03 +0200

    practice

--- js/task-1.js
+++ js/task-1.js
@@ -10,21 +10,4 @@ console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credi
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
 
 
-function normalizeInput(input, to) {
-  if (to === "upper") {
-    return input.toUpperCase();
-  } else {
-    return input.toLowerCase();
-  }
-}
 
-console.log(normalizeInput("This ISN'T SpaM", "lower")); // "this isn't spam"
-console.log(normalizeInput("This ISN'T SpaM", "upper")); // "THIS ISN'T SPAM"
-console.log(normalizeInput("Big SALE", "lower")); // "big sale"
-console.log(normalizeInput("Big SALE", "upper")); // "BIG SALE"
-console.log(normalizeInput("Stay Awhile and Listen", "lower")); // "stay awhile and listen"
-console.log(normalizeInput("Stay Awhile and Listen", "upper")); // "STAY AWHILE AND LISTEN"
-
-function checkForName(fullName, firstName) {
-  return fullName.toLowerCase().includes(firstName.toLowerCase());
-}
