00d2b54ee0f7d00f059573b1ff47a9b4dc177584
Katrinast <stehnovicekaterina@gmail.com>
2024-03-26 21:53:22 +0200

    practice

--- js/task-1.js
+++ js/task-1.js
@@ -478,4 +478,11 @@
 
 // person.introduce();
 
-
+const object = {
+  message: 'Hello, World',
+  getMessage() {
+    const message = 'Hello, Earth';
+    return this.message;
+  },
+};
+console.log(object.getMessage());
