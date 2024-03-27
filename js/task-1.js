7138fa754f9a9e4fb01449080853cc9714ea7a4d
Katrinast <stehnovicekaterina@gmail.com>
2024-03-27 21:03:47 +0200

    practice

--- js/task-1.js
+++ js/task-1.js
@@ -478,11 +478,39 @@
 
 // person.introduce();
 
-const object = {
-  message: 'Hello, World',
-  getMessage() {
-    const message = 'Hello, Earth';
-    return this.message;
-  },
-};
-console.log(object.getMessage());
+// const object = {
+//   message: 'Hello, World',
+//   getMessage() {
+//     const message = 'Hello, Earth';
+//     return this.message;
+//   },
+// };
+// console.log(object.getMessage());
+
+class Car {
+  #price;
+  static maxPrice = 50000;
+
+  constructor(params) {
+    this.#price = params.price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    if (newPrice <= Car.maxPrice) {
+    this.#price = newPrice;
+  }
+  }
+  }
+
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
